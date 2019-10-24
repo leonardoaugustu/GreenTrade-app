85c2c8c2a3a571fc2b17f1e61a61cde0b88d5a37
Leonardo <leonardo.augustu@gmail.com>
2019-10-24 00:17:11 -0400

    A client can log in with their account details or social media (U1)
    A client can log in with their account details or social media (U1).
    
    Code API calls for account;
    Code input/error validation;

---
+++ config/FireBaseConfig.js
@@ -0,0 +1,15 @@
+import { YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Setting a timer', 'Accessing view manager']);
+
+// Your web app's Firebase configuration
+  export default firebaseConfig = {
+    apiKey: "AIzaSyCk-SaxbmyTEHiv59jvMzX2AYuhlt15SfU",
+    authDomain: "react-native-firebase-801e3.firebaseapp.com",
+    databaseURL: "https://react-native-firebase-801e3.firebaseio.com",
+    projectId: "react-native-firebase-801e3",
+    storageBucket: "react-native-firebase-801e3.appspot.com",
+    messagingSenderId: "1053830574114",
+    appId: "1:1053830574114:web:f603ae9f5ffe4b90535a7b",
+    measurementId: "G-10K5GR9T3Q"
+  };
