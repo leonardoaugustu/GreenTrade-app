cea1a3fef8a6b33c46b641f193881dfbcb610d5b
jo <jay.jo.dev@gmail.com>
2019-11-11 12:33:18 -0500

    add unit test for checking text in SignIn View

--- navigations/SignInScreen/SignInView.test.js
+++ navigations/SignInScreen/SignInView.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import SignInView from './SignInView';
 import SafeAreaView from "react-native-safe-area-view";
 import { KeyboardAvoidingView } from 'react-native';
@@ -13,3 +13,13 @@ beforeEach(() => {
 it('has Keyboard Avoiding View', () => {
   expect(wrapped.find(KeyboardAvoidingView).length).toEqual(1);
 });
+
+// Check SignInView includes GreenTrade text
+it('has a GreenTrade text ', () => {
+  expect(wrapped.find("Text").first().props().children).toEqual('GreenTrade')
+})
+
+// Check SignInView includes Sign In With Google text
+it('has a Sign In With Google text ', () => {
+    expect(wrapped.find("Text").last().props().children).toEqual('Sign In With Google')
+})
