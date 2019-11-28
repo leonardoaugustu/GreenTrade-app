1a0cd1161196bf454c117640216e119ae14b147a
HarleyQuinna <sylviachen627@gmail.com>
2019-11-28 16:28:30 -0500

    updated

---
+++ navigations/InitialHomeScreen/InitialView.js
@@ -0,0 +1,92 @@
+import React, { Component } from "react";
+import { Text, View, Image, TouchableOpacity, Button} from "react-native";
+import { Icon } from "react-native-elements";
+import styles from "./styles";
+import SafeAreaView from "react-native-safe-area-view";
+import { Dialog } from 'react-native-simple-dialogs';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
+
+export default class InitialView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dialogVisible: false,
+    };
+  }
+
+  toggleCamera = () => {
+    this.setState({dialogVisible: true})
+  }
+  _canceltoggle = () => {
+    this.setState({dialogVisible: false})
+  }
+
+  async componentDidMount() {
+		await Permissions.askAsync(Permissions.CAMERA_ROLL);
+		await Permissions.askAsync(Permissions.CAMERA);
+  }
+  
+  _takePhoto = async () => {
+		let pickerResult = await ImagePicker.launchCameraAsync({
+			allowsEditing: true,
+			aspect: [4, 3]
+		});
+
+		this._handleImagePicked(pickerResult);
+	};
+
+	_pickImage = async () => {
+		let pickerResult = await ImagePicker.launchImageLibraryAsync({
+			allowsEditing: true,
+			aspect: [4, 3]
+		});
+
+		this._handleImagePicked(pickerResult);
+	};
+
+  render() {
+ 
+    return (
+        <SafeAreaView style={styles.container}>
+           <View style={styles.headerContainer}>
+              <View style={styles.header}>
+              <View style={styles.iconWrapper}>
+              <Icon
+          onPress={() => this.props.navigation.openDrawer()}
+          type="material"
+          name="menu"
+          size={30}
+          color="#fff"
+          containerStyle={styles.drawerIcon}
+        />
+              </View>
+              <View style={styles.titleWrapper}>
+                  <Text style={styles.textTitle}>Green Trade</Text>
+              </View>
+              </View>
+          </View>
+          <TouchableOpacity onPress={this.toggleCamera}>
+          <View style={styles.cameraWrapper}>
+          <Image style={styles.cameraImg} source={require('../../assets/camera.png')}/>
+          </View>
+          </TouchableOpacity>
+          <View style={styles.dialogContainer}>
+                <Dialog
+                dialogStyle ={styles.dialog}
+                   visible={this.state.dialogVisible}
+                   >
+                       <View style={styles.customDialog}>
+                       <Button
+							onPress={this._pickImage}
+							title="Choose from camera roll"
+						/>
+						<Button onPress={this._takePhoto} title="Take a photo" />
+            <Button onPress={this._canceltoggle} title="Cancel" />
+                       </View>
+                </Dialog>
+                </View>
+        </SafeAreaView>
+    );
+  }
+}
---
+++ navigations/InitialHomeScreen/styles.js
@@ -0,0 +1,101 @@
+import { StyleSheet } from 'react-native';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+
+export default StyleSheet.create({
+    menuContainer: {
+        flex: 1,
+        flexDirection: "column",
+        backgroundColor: '#fff',
+        width: wp('68%')
+    },
+    safeView: {
+        flexDirection: "column",
+    },
+    profileContainer:{
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: wp('0%'),
+        height: hp('30%'),
+        backgroundColor: '#AFE2FC',
+    },
+   profileImg: {
+        width: wp('22%'),
+        height: wp('22%'),
+        borderRadius: wp('11%'),
+        marginTop: wp('8%')
+   },
+   nameTxt: {
+       marginTop: wp('2%'),
+       fontSize: wp('5%')
+   },
+   emailTxt:{
+    marginTop: wp('2%'),
+    color: 'dimgrey'
+   },
+   DrawerComponentScrollView: {
+    // marginTop: hp('5"%'),
+  },
+  menuItem: {
+     justifyContent:'center',
+    alignSelf: 'center'
+  },
+  menuIcon: {
+      fontSize: wp('5%')
+  },
+  container: {
+    flex: 1,
+    flexDirection: "column",
+},
+headerContainer: {
+    flexDirection: "row",
+    width: wp('100%'),
+    height: hp('8.4%'),
+    backgroundColor: '#87D5FA',    
+},
+header: {
+    flexDirection: "row",
+    width: wp('100%'),
+    height: hp('9.4%'),
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+iconWrapper: {
+    flex: 0.5,
+},
+titleWrapper: {
+    flex: 2,
+},
+textTitle: {
+    fontSize: wp('5%'),
+    marginLeft: wp('18%'),
+    color: "#fff"
+},
+drawerIcon: {
+    marginLeft: wp('-6%')
+},
+cameraWrapper: {
+    marginTop: hp('60%'),
+    width: wp('100%'),
+    justifyContent: 'center',
+    alignItems: 'center',
+},
+cameraImg: {
+    alignSelf: 'center',
+    width: wp('35%'),
+    height: wp('35%')
+},
+dialogContainer: {
+    width: wp('100%'),
+    justifyContent: 'center',
+    alignItems: 'center'
+},
+dialog: {
+    width: wp('70%'),
+    borderRadius: wp('10%'),
+    alignSelf: 'center'
+},
+customDialog: {
+    flexDirection: 'column',
+    height: hp('15%'),
+},
+})
