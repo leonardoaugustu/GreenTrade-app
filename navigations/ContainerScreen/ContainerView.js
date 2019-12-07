a0a9f0d9573dd62c6747d9b4c00e91e929e9600f
Frank <frankzhou@Hotmail.ca>
2019-12-06 23:30:01 -0500

    Added scrolling refresh for pickups and containers view.

--- navigations/ContainerScreen/ContainerView.js
+++ navigations/ContainerScreen/ContainerView.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity, ActivityIndicator } from "react-native";
+import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity, ActivityIndicator, RefreshControl } from "react-native";
 import { List, ListItem, Icon, Image } from "react-native-elements";
 import styles from "./styles";
 import SafeAreaView from "react-native-safe-area-view";
@@ -11,8 +11,9 @@ export default class ContainerView extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: true,
       containerData: [],
-      loading: true
+      loading: true,
     }
   }
 
@@ -24,11 +25,12 @@ export default class ContainerView extends Component {
     });
   }
 
-  fetchData() {
+  fetchData = () => {
     try {
       const newData = [];
       var db = firebase.firestore();
 
+      this.setState({ isLoading: true});
       db.collection("users")
         .doc(firebase.auth().currentUser.uid)
         .collection('containers')
@@ -49,15 +51,14 @@ export default class ContainerView extends Component {
                   orderedDate: doc.data().orderedDate
                 };
                 newData.push(containerInfo);
-                this.setState({ containerData: newData, loading: false});
-
+                this.setState({ containerData: newData, loading: false, isLoading: false});
               });
           });
         });
     }
     catch (error) {
       console.log(error);
-    }    
+    }
   }
 
   renderSeparator = () => {
@@ -116,7 +117,6 @@ export default class ContainerView extends Component {
             <View style={styles.displayBox}>
               <Text style={styles.displayText}>Ordered Containers</Text>
             </View>
-
             <FlatList
               style={styles.list}
               data={this.state.containerData.sort((b,a) => (a.orderedDate.toDate() > b.orderedDate.toDate()) - (a.orderedDate.toDate() < b.orderedDate.toDate() )) }
@@ -126,6 +126,8 @@ export default class ContainerView extends Component {
               extraData={this.state}
               removeClippedSubviews={false}
               ListEmptyComponent={this.renderEmptyList}
+              refreshing={this.state.isLoading}
+              onRefresh={this.fetchData}
             />
           </View>
         }
