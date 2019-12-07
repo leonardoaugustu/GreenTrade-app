f663ec898d0262c234128f10ca9cff8787968a8e
Frank <frankzhou@Hotmail.ca>
2019-12-07 03:28:27 -0500

    Revamped the view for pickup history

--- navigations/CollectorPickupHistory/CollectorPickupHistoryView.js
+++ navigations/CollectorPickupHistory/CollectorPickupHistoryView.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity } from "react-native";
-import { Icon, Image } from "react-native-elements";
+import { Text, View, StyleSheet, Button, FlatList, TouchableOpacity, ActivityIndicator } from "react-native";
+import { Icon, Image, Card, ListItem } from "react-native-elements";
 import styles from "./styles";
 import SafeAreaView from "react-native-safe-area-view";
 import 'firebase/firestore';
@@ -8,98 +8,120 @@ import 'firebase/firestore';
 import firebaseConfig from '../../config/FireBaseConfig'
 import firebase from '../../config/firebase'
 import { Dropdown } from 'react-native-material-dropdown';
+import moment from 'moment';
 
-export default class CollectorPickupHistoryView extends Component 
-{
-    constructor(props) 
-    {
-        super(props);
-        
-        this.state = {collectorHistoryData:[] }
-      }
+export default class CollectorPickupHistoryView extends Component {
+  constructor(props) {
+    super(props);
 
-      componentDidMount()
-      {
-        try{
-          const newData=[]; 
-          var db = firebase.firestore();
-    
-      
-          db.collection("completed-pickups").doc(firebase.auth().currentUser.uid).collection("pickups").get().then((querySnapshot) => {
-            querySnapshot.forEach((doc) => 
-            {
+    this.state = {
+      collectorHistoryData: [],
+      isLoading: true,
+    }
 
-                var historyInfo =
-                  {
-                    "Url": doc.data().url,
-                    "Timestamp": doc.data().time
-                  };
-                newData.push(historyInfo);
-              this.setState({collectorHistoryData: newData});
-              //console.log(this.state.collectorHistoryData);
-              //console.log(firebase.auth().currentUser.uid)
-            });
-          });
-        }
-        catch (error){
-          console.log(error);
-        }
-      }
+  }
 
-      renderSeparator = () => {
-        return (
-          <View
-            style={{
-              height: 2,
-              width: "100%",
-              backgroundColor: "#000000",
-            }}
-          />
-        );
-      };
-      renderItem = ({ item}) => (
-        <TouchableOpacity >
-        <View style={{ flex: 1, flexDirection: 'row' }}>
-        <Image source={{ uri: item.Url}} style={styles.imgContainer}/>
+  componentDidMount() {
+    this.fetchData();
+  }
 
-          <Text style={{ flex: 1, fontSize: 18, textAlign: 'right',  textAlignVertical: "center" }}>Date: {item.Timestamp}</Text>      
-        </View>
-        </TouchableOpacity>
-      );
+  fetchData = () => {
+    try {
+      const newData = [];
+      var db = firebase.firestore();
 
-      render()
-      {
-        return(
-          <SafeAreaView style={styles.container}>
-          <View style={styles.headerContainer}>
-            <View style={styles.header}>
-              <View style={styles.iconWrapper}>
-                <Icon
-                  onPress={() => this.props.navigation.openDrawer()}
-                  type="material"
-                  name="menu"
-                  size={30}
-                  color="#fff"
-                  containerStyle={styles.drawerIcon}
-                />
-              </View>
-              <View style={styles.titleWrapper}>
-                <Text style={styles.textTitle}>Pickup History</Text>
-    
-              </View>
+      this.setState({ isLoading: true });
+      db.collection("completed-pickups").doc(firebase.auth().currentUser.uid).collection("pickups").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          var historyInfo =
+          {
+            imageUri: doc.data().imageUri,
+            fulfilledTime: doc.data().fulfilledTime,
+            address: doc.data().address,
+            memberNotes: doc.data().memberNotes,
+          };
+          newData.push(historyInfo);
+          this.setState({ collectorHistoryData: newData });
+        });
+      });
+    }
+    catch (error) {
+      console.log(error);
+    }
+    finally {
+      this.setState({ isLoading: false });
+    }
+  }
+
+  renderSeparator = () => {
+    return (
+      // <View style={{  
+      //   flex: 1,
+      //   height: 1,
+      //   backgroundColor: "#e4e4e4",
+      //   marginLeft: 25,
+      //   marginRight: 25,
+      // }}
+      //  />
+      null
+    );
+  };
+
+  renderItem = ({ item }) => (
+    <Card
+      containerStyle={styles.cardContainer}
+      //title={item.address.street}
+      image={{ uri: item.imageUri }}
+      placeholder={<ActivityIndicator size="large" color="#00ff00" />}
+      >
+      <ListItem
+        containerStyle={styles.cardContainer}
+        title={`${item.address.street}`}
+        subtitle={`Notes: ${item.memberNotes}`}
+        rightSubtitle={moment(item.fulfilledTime.toDate()).format('MMM Do, h:mm a')}
+      />
+    </Card>
+  );
+
+  renderEmptyList = () => {
+    return (
+      <Text style={styles.displayMessage}>No Completed Pickups Found.</Text>
+    );
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <View style={styles.headerContainer}>
+          <View style={styles.header}>
+            <View style={styles.iconWrapper}>
+              <Icon
+                onPress={() => this.props.navigation.openDrawer()}
+                type="material"
+                name="menu"
+                size={30}
+                color="#fff"
+                containerStyle={styles.drawerIcon}
+              />
+            </View>
+            <View style={styles.titleWrapper}>
+              <Text style={styles.textTitle}>Completed Pickups</Text>
+
             </View>
           </View>
-          <FlatList
-        data={this.state.collectorHistoryData}
-        ItemSeparatorComponent={this.renderSeparator}
-        renderItem={this.renderItem}
-       keyExtractor={item => item.Url}
-        extraData={this.state}
-        removeClippedSubviews={false}
-        style={{flex:1}}
-      >
-      </FlatList>
-        </SafeAreaView>
-      );
-      }
+        </View>
+        <FlatList
+          data={this.state.collectorHistoryData}
+          ItemSeparatorComponent={this.renderSeparator}
+          renderItem={this.renderItem}
+          keyExtractor={item => item.imageUri}
+          extraData={this.state}
+          ListEmptyComponent={this.renderEmptyList}
+          refreshing={this.state.isLoading}
+          onRefresh={this.fetchData}
+        >
+        </FlatList>
+      </SafeAreaView>
+    );
+  }
 }
